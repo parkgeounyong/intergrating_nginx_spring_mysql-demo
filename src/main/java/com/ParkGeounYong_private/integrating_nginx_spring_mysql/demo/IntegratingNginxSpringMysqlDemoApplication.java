5d0db0b3ef8eacfe3c06204c16f5be08ff2b0673
박근용 <qkrnsdyd1@gmail.com>
2023-05-17 10:34:52 +0900

    깃 연동

---
+++ src/main/java/com/ParkGeounYong_private/integrating_nginx_spring_mysql/demo/IntegratingNginxSpringMysqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ParkGeounYong_private.integrating_nginx_spring_mysql.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratingNginxSpringMysqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegratingNginxSpringMysqlDemoApplication.class, args);
+	}
+
+}
